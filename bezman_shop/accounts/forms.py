310e380a8ab773364f5d374777ff9cfa3ad4dc5b
Mekazara <mekuchazara@gmail.com>
2020-12-03 12:50:40 +0600

    created forms

---
+++ bezman_shop/accounts/forms.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.forms import UserCreationForm
+from .models import *
+
+# class CustomerForm(UserCreationForm):
+#     class Meta:
+#         model = Customer
+#         fields = '__all__'
