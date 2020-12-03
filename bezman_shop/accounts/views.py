95bf6619db7b6641b2e7c2f78fa45550b6c2c8b5
Mekazara <mekuchazara@gmail.com>
2020-12-03 12:52:55 +0600

    new views created

--- bezman_shop/accounts/views.py
+++ bezman_shop/accounts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from .models import *
+
 
 # Create your views here.
 
@@ -8,7 +10,23 @@ def customerList(request):
     context = {'customers': customers}
     return render(request, 'accounts/customers.html', context)
 
+
 def getCustomer(request, customer_id):
     customer = Customer.objects.get(id=customer_id)
-    context = {'customer': customer}
+    orders = customer.orders_set.all()
+    context = {'customer': customer, 'orders': orders}
     return render(request, 'accounts/getcustomer.html', context)
+
+
+def createUser(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('customers')
+    context = {'form': form}
+    return render(request, 'accounts/user-create.html', context)
+
+# def createCustomer(request):
+#     form =
