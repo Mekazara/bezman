6bc2ec96665b6cb0dd9265cfaa8d9e2447f12808
Mekazara <mekuchazara@gmail.com>
2020-12-11 12:27:11 +0600

    new url to get access

--- bezman_shop/bezman_shop/urls.py
+++ bezman_shop/bezman_shop/urls.py
@@ -17,12 +17,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from supershop.views import productList
 
 urlpatterns = [
                   path('jet/', include('jet.urls', 'jet')),
                   path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
                   path('admin/', admin.site.urls),
+                  path('', productList, name='products'),
                   path('shop/', include('supershop.urls')),
                   path('accounts/', include('accounts.urls')),
-
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- bezman_shop/supershop/urls.py
+++ bezman_shop/supershop/urls.py
@@ -5,8 +5,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('products/', productList, name='products'),
     path('orders/', orderList, name='orders'),
     path('ordercreate/<int:product_id>/', orderCreate, name='ordercreate'),
-    path('orderupdate/<int:order_id>/', orderUpdate, name='orderupdate')
+    path('orderupdate/<int:order_id>/', orderUpdate, name='orderupdate'),
+    path('orderdelete/<int:order_id>/', orderDelete, name='orderdelete'),
 ]
